cf071d2868baf28d8b3aa7544052f0eee9a546da
linux-acs4096 <2405434133@qq.com>
2026-04-23 17:16:17 +0800

    优化冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -5,17 +5,24 @@ using namespace std;
 //Linux 给sort添加注释
 void sort(int arr[],int size){
     int tmp = 0;
+    bool flag = false;
 	for(int i = 0; i < size - 1; ++i)
 	{
 		for(int j = 0 ; j < size - i; ++j)
 		{
 			if(arr[j] > arr[j + 1])
 			{
-				tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
+				    tmp = arr[j];
+				    arr[j] = arr[j + 1];
+				    arr[j + 1] = tmp;
+                    flag = true;
 			}
 		}
+
+        if(!flag)
+        {
+            break;
+        }
 		
 	}
 }
